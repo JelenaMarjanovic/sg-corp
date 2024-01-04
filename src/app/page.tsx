4e91b77a7f672993ca799d23459018911194da39
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2024-01-04 11:55:51 +0100

    Adding images - basic setup with basic styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import Image from 'next/image';
+import homeImg from '/public/home.jpg';
+
 export default function HomePage() {
-  return <div>Home Page</div>;
+  return <div>
+    Home Page
+    <div className="absolute -z-10 inset-0">
+      <Image
+        src={homeImg}
+        alt="car factory"
+        fill
+        style={{ objectFit: 'cover' }}
+      />
+    </div>
+  </div>;
 }
