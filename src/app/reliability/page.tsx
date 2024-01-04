f67d96ddb0e4ecbeb7fc8d6464ae831782b4b91a
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2024-01-04 14:56:21 +0100

    Adding a reusable presentation component - Hero + some basic styling

--- src/app/reliability/page.tsx
+++ src/app/reliability/page.tsx
@@ -1,3 +1,12 @@
+import reliabilityImg from '/public/reliability.jpg';
+import Hero from '@/components/hero';
+
 export default function ReliabilityPage() {
-  return <div>Reliability Page</div>;
+  return (
+    <Hero
+      imgData={reliabilityImg}
+      imgAlt="manual welding"
+      title="Reliability is our number one priority"
+    />
+  );
 }
