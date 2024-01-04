b5c88b51b0af4a64318fb7cc63b6343a250eaf58
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2024-01-04 01:24:10 +0100

    Common UI in NextJS with Layouts + Home Page + clean global CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import Link from 'next/link';
-
 export default function HomePage() {
-  return <div>
-    <div>
-      <Link href="/performance">Performance</Link>
-      <Link href="/reliability">Reliability</Link>
-      <Link href="/scale">Scale</Link>
-    </div>
-    Home Page
-  </div>;
+  return <div>Home Page</div>;
 }
